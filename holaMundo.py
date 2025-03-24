b83a6e83900fa4a9b9f9cbdf0d4ff1da7b7dbc08
Ezequiel <eze.xray@gmail.com>
2025-03-24 09:17:06 -0300

    se agrego codigo a HolaMundo.py

--- holaMundo.py
+++ holaMundo.py
@@ -1,4 +1,4 @@
-print("hola")
-print("mundo")
-variable1 = 10
-variable2 = 20
+nombre = input("ingrese nombre: ")
+apellido = input("ingrese apellido: ")
+variable1 = int(input("ingrese un numero: "))
+variable2 = int(input("ingrese otro numero: "))
